eb467ccc5f92d820de898b8c07e2266f3d25a0d1
lbdvriesGT <80513371+lbdvriesGT@users.noreply.github.com>
2021-11-24 14:43:52 +0100

    Update __init__.py

--- cardio/models/__init__.py
+++ cardio/models/__init__.py
@@ -1,6 +1,5 @@
 """Contains ECG models and custom functions."""
 
-from .dirichlet_model import *  # pylint: disable=wildcard-import
 from .fft_model import *  # pylint: disable=wildcard-import
 from .hmm import *  # pylint: disable=wildcard-import
 from . import metrics
