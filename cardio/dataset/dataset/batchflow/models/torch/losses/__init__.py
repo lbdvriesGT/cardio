9f1eb1d7ef6da05308cce7f0e83405a37295a7ea
lbdvriesGT <80513371+lbdvriesGT@users.noreply.github.com>
2021-11-24 13:53:57 +0100

    dataset name

--- cardio/dataset/dataset/batchflow/models/torch/losses/__init__.py
+++
@@ -1,7 +0,0 @@
-""" Custom losses """
-#pylint: disable=wildcard-import
-from .core import CrossEntropyLoss
-from .binary import *
-from .multiclass import *
-from .lovasz import *
-from .ssim import *
