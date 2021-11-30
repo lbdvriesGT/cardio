6923392b3a934f7c5e9a44c5798e552127b15439
lbdvriesGT <80513371+lbdvriesGT@users.noreply.github.com>
2021-11-30 13:10:41 +0100

    Added basebatch

--- cardio/dataset/dataset/batch.py
+++ cardio/dataset/dataset/batch.py
@@ -1,10 +1,31 @@
-Skip to content
-Search or jump to…
-Pull requests
-Issues
-Marketplace
-Explore
- 
+
+######
+""" Contains the base batch class """
+from binascii import hexlify
+import numpy as np
+
+
+class BaseBatch:
+    """ Base class for batches
+    Required to solve circular module dependencies
+    """
+    def __init__(self, index, *args, **kwargs):
+        _ = args, kwargs
+        self.index = index
+        self._data_named = None
+        self._data = None
+        self.pipeline = None
+
+    @staticmethod
+    def make_filename():
+        """ Generate unique filename for the batch """
+        random_data = np.random.uniform(0, 1, size=10) * 123456789
+        # probability of collision is around 2e-10.
+        filename = hexlify(random_data.data)[:8]
+        return filename.decode("utf-8")
+
+
+######
 import os
 import threading
 
@@ -30,7 +51,6 @@ except ImportError:
 from .dsindex import DatasetIndex, FilesIndex
 from .decorators import action, inbatch_parallel, any_action_failed
 from .dataset import Dataset
-from .batch_base import BaseBatch
 from .components import MetaComponentsTuple
 
 
